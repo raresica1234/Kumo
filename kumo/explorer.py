9fbe54325da6c37511c401751a473c5fc78bf292
rares <colorlord98@gmail.com>
2020-10-07 13:51:20 +0300

    Explorer now checks for read permission

--- kumo/explorer.py
+++ kumo/explorer.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
+import kumo.media_directories as media
 import os
 
 bp = Blueprint("explorer", __name__)
@@ -7,11 +8,12 @@ bp = Blueprint("explorer", __name__)
 @bp.route("/")
 def index():
     file_data = []
-    current_path = "/WIN_D/"
-    files = os.listdir(current_path)
-    for file in files:
-        file_type = os.path.isdir(os.path.join(current_path, file))
-        file_data.append((file, file_type))
+    current_path = "/WIN_D"
+    if media.check_read_permission(current_path):
+        files = os.listdir(current_path)
+        for file in files:
+            file_type = os.path.isdir(os.path.join(current_path, file))
+            file_data.append((file, file_type))
 
     return render_template("index.html", files=file_data, baseurl="")
 
@@ -19,10 +21,11 @@ def index():
 @bp.route("/explorer/<path:subpath>")
 def explore(subpath):
     file_data = []
-    current_path = os.path.join("/WIN_D/", subpath)
-    files = os.listdir(current_path)
-    for file in files:
-        file_type = os.path.isdir(os.path.join(current_path, file))
-        file_data.append((file, file_type))
+    current_path = os.path.join("/WIN_D", subpath)
+    if media.check_read_permission(current_path):
+        files = os.listdir(current_path)
+        for file in files:
+            file_type = os.path.isdir(os.path.join(current_path, file))
+            file_data.append((file, file_type))
 
     return render_template("index.html", file_data=file_data, baseurl=subpath)
