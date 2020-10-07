535246ab4929a07cdbdee8d42560d7f91d2f3970
rares <colorlord98@gmail.com>
2020-10-07 11:43:22 +0300

    Added capability of differentiating between directories and files

--- kumo/explorer.py
+++ kumo/explorer.py
@@ -6,13 +6,23 @@ bp = Blueprint("explorer", __name__)
 
 @bp.route("/")
 def index():
-    files = os.listdir("/WIN_D")
-    return render_template("index.html", files=files, baseurl="")
+    file_data = []
+    current_path = "/WIN_D/"
+    files = os.listdir(current_path)
+    for file in files:
+        file_type = os.path.isdir(os.path.join(current_path, file))
+        file_data.append((file, file_type))
+
+    return render_template("index.html", files=file_data, baseurl="")
 
 
 @bp.route("/explorer/<path:subpath>")
 def explore(subpath):
-    files = os.listdir(os.path.join("/WIN_D/", subpath))
-    print(subpath)
+    file_data = []
+    current_path = os.path.join("/WIN_D/", subpath)
+    files = os.listdir(current_path)
+    for file in files:
+        file_type = os.path.isdir(os.path.join(current_path, file))
+        file_data.append((file, file_type))
 
-    return render_template("index.html", files=files, baseurl=subpath)
+    return render_template("index.html", file_data=file_data, baseurl=subpath)
