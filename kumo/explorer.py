a91c512fc21e5205ed7b673dad61ea729fe3ab4e
rares <colorlord98@gmail.com>
2020-10-07 23:22:34 +0300

    Explorer now uses the new permission system

--- kumo/explorer.py
+++ kumo/explorer.py
@@ -1,6 +1,6 @@
 import os
-import kumo.media_directories as media
-from flask import Blueprint, jsonify
+from kumo.unix_permissions import UnixPermissions
+from flask import Blueprint, jsonify, g
 
 bp = Blueprint("explorer", __name__)
 
@@ -8,19 +8,24 @@ bp = Blueprint("explorer", __name__)
 @bp.route("/explore")
 @bp.route("/explore/<path:subpath>")
 def explore(subpath=None):
+    if "permission" not in g:
+        g.permission = UnixPermissions()
+
     file_data = []
     files = []
     if subpath is None:
-        files = media.get_readable_directories()
+        files = g.permission.get_readable_root_directories(g.user.id, g.user.admin)
         for file in files:
             file_type = os.path.isdir(file)
             file_data.append((file, file_type))
     else:
         current_path = "/" + subpath
-        if media.check_read_permission(current_path):
+        if g.permission.has_permission(current_path, g.user.id, g.user.admin):
             files = os.listdir(current_path)
         for file in files:
             file_type = os.path.isdir(os.path.join(current_path, file))
-            file_data.append((file, file_type))
+            if file_type:
+                if g.permission.has_permission(os.path.join(current_path, file), g.user.id, g.user.admin):
+                    file_data.append((file, file_type))
 
     return jsonify(file_data)
