8be854dcb3b98c8945cb8a9f86846f69e2c8282c
rares <colorlord98@gmail.com>
2020-10-07 13:51:03 +0300

    Added a check read permission function

--- kumo/media_directories.py
+++ kumo/media_directories.py
@@ -1,9 +1,23 @@
 import click
-from os import path
 from flask.cli import with_appcontext
-from flask import current_app
+from flask import current_app, g
 from kumo import config
-from kumo.models import MediaDirectory, db
+from kumo.models import MediaDirectory, MediaPermissions, db
+
+
+def check_read_permission(directory):
+    if g.user is None:
+        return False
+    if g.user.admin == 1:
+        return True
+    else:
+        permissions = MediaPermissions.query.filter_by(user_id=g.user.id).all()
+        media_dirs = []
+        for permission in permissions:
+            if permission.read_permission:
+                media_dirs.append(MediaDirectory.query.filter_by(id=permission.media_id))
+
+        return False
 
 
 @click.command("update-media")
