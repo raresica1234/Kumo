44c753ba4249eba83af4dcb6ebaeca940b77c3b0
rares <colorlord98@gmail.com>
2020-10-17 13:43:53 +0300

    Added way of navigating through the ffolders as client side rendering Added way of remembering current location via cookies

--- kumo/static/explorer.js
+++ kumo/static/explorer.js
@@ -1,19 +1,47 @@
-let request = new XMLHttpRequest()
+const COOKIE_LOCATION_NAME = "location";
 
-request.open('GET', document.URL + "explore", true)
+function getCookieLocation() {
+    let cookies = decodeURIComponent(document.cookie).split(";");
 
-request.onload = function() {
-    let data = JSON.parse(this.response)
+    let currentPath = "";
 
-    let content = ""
-
-    for (let i = 0; i < data.length; i++) {
-        content += "<h1> " + data[i][0] + ", Type: " + (data[i][1] ? "directory" : "file") + "</h1>"
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        if (cookie.startsWith(" "))
+            cookie = cookie.substr(1);
+        if (cookie.startsWith(COOKIE_LOCATION_NAME + "="))
+            currentPath = cookie.substr(COOKIE_LOCATION_NAME.length + 1);
     }
 
-    document.getElementById("explorer").innerHTML = content
-
-    console.log(data)
+    return currentPath;
 }
 
-request.send()
+function clearCookie() {
+    document.cookie = COOKIE_LOCATION_NAME + "=; Secure";
+}
+
+function explore(location) {
+    document.cookie = COOKIE_LOCATION_NAME + "=" + location + "; Secure";
+    fetch('explore/' + location)
+    .then(res => res.json())
+    .then(data => process_request(data));
+}
+
+function process_request(data) {
+    let currentPath = getCookieLocation();
+    if (currentPath !== "")
+        currentPath += "/";
+
+    let content = "";
+
+    for (let i = 0; i < data.length; i++)
+        content += "<h1><a href='javascript:explore(\"" + currentPath + data[i][0] + "\")'>" + data[i][0] + ", Type: " + (data[i][1] ? "directory" : "file") + "</a></h1>";
+
+    document.getElementById("explorer").innerHTML = content;
+}
+
+let previousLocation = getCookieLocation()
+
+fetch(decodeURIComponent(document.URL + "explore" + previousLocation))
+    .then(res => res.json())
+    .then(data => process_request(data));
