a8b06767d6f81a15bfa765b1799aaa50aeda1557
rares <colorlord98@gmail.com>
2020-12-17 16:59:17 +0200

    Fixed error regarding not checking if user is actually defined

--- kumo/explorer.py
+++ kumo/explorer.py
@@ -13,22 +13,23 @@ def explore(subpath=None):
 
     file_data = []
     files = []
-    if subpath is None:
-        files = g.permission.get_readable_root_directories(g.user.id, g.user.admin)
-        for file in files:
-            file_type = os.path.isdir(file)
-            file_data.append((file, file_type))
-    else:
-        current_path = "/" + subpath
-        if os.path.exists(current_path) and os.path.isdir(current_path):
-            if g.permission.has_permission(current_path, g.user.id, g.user.admin):
-                files = os.listdir(current_path)
+    if g.user is not None:
+        if subpath is None:
+            files = g.permission.get_readable_root_directories(g.user.id, g.user.admin)
             for file in files:
-                file_type = os.path.isdir(os.path.join(current_path, file))
-                if file_type:
-                    if g.permission.has_permission(os.path.join(current_path, file), g.user.id, g.user.admin):
+                file_type = os.path.isdir(file)
+                file_data.append((file, file_type))
+        else:
+            current_path = "/" + subpath
+            if os.path.exists(current_path) and os.path.isdir(current_path):
+                if g.permission.has_permission(current_path, g.user.id, g.user.admin):
+                    files = os.listdir(current_path)
+                for file in files:
+                    file_type = os.path.isdir(os.path.join(current_path, file))
+                    if file_type:
+                        if g.permission.has_permission(os.path.join(current_path, file), g.user.id, g.user.admin):
+                            file_data.append((file, file_type))
+                    else:
                         file_data.append((file, file_type))
-                else:
-                    file_data.append((file, file_type))
 
     return jsonify(file_data)
