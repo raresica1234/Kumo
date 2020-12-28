cd8ea6d441eece28a74641a85eb4f34a1aa22862
rares <colorlord98@gmail.com>
2020-12-28 19:57:20 +0200

    Changed exploring to fit the new way of exploring

--- kumo/explorer.py
+++ kumo/explorer.py
@@ -7,21 +7,23 @@ bp = Blueprint("explorer", __name__)
 
 @bp.route("/explore/")
 @bp.route("/explore")
-@bp.route("/explore/<path:subpath>")
-def explore(subpath=None):
+@bp.route("/explore/<path:sub_path>")
+def explore(sub_path=None):
     if "permission" not in g:
         g.permission = UnixPermissions()
 
     file_data = []
     files = []
     if g.user is not None:
-        if subpath is None:
-            files = g.permission.get_readable_root_directories(g.user.id, g.user.admin)
-            for file in files:
-                file_type = os.path.isdir(file)
-                file_data.append((file, file_type))
+        if sub_path is None:
+            directories = g.permission.get_readable_root_directories(g.user.id, g.user.admin)
+            for name, path in directories:
+                file_type = os.path.isdir(path)
+                file_data.append((name, file_type))
         else:
-            current_path = "/" + subpath
+            root_directory_name = sub_path.split("/")[0]
+            root_directory_path = g.permission.get_root_directory_path(root_directory_name)
+            current_path = root_directory_path + sub_path[len(root_directory_name):]
             if os.path.exists(current_path) and os.path.isdir(current_path):
                 if g.permission.has_permission(current_path, g.user.id, g.user.admin):
                     files = os.listdir(current_path)
