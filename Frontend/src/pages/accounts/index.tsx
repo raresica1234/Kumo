4278d60a3acb9bed37468d8ecd22ee23f261d53e
BuildTools <colorlord98@gmail.com>
2022-02-28 17:14:40 +0200

    Use paper instead of hard coding background color for page

--- Frontend/src/pages/accounts/index.tsx
+++ Frontend/src/pages/accounts/index.tsx
@@ -1,11 +1,12 @@
 import {Navigate, Route, Routes} from "react-router-dom";
 import Register from "./register";
 import CenterContainer from "../../components/center-container";
+import {Paper} from "@mui/material";
 
 const AccountRoutes = () => (
 	<Routes>
 		{/*<Route path="/login" element={<Login />}/>*/}
-		<Route path="/register" element={<CenterContainer><Register/></CenterContainer>}/>
+		<Route path="/register" element={<Paper><CenterContainer><Register/></CenterContainer></Paper>}/>
 		<Route path="*" element={<Navigate to="/register"/>}/>
 	</Routes>
 );
