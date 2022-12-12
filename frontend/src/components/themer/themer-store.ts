61260b94c15cc07f6139fdb259f684c6173c3797
raresica1234 <colorlord98@gmail.com>
2022-12-12 16:38:38 +0200

    Added postgre database

--- frontend/src/components/themer/themer-store.ts
+++ frontend/src/components/themer/themer-store.ts
@@ -3,7 +3,7 @@ import {createContext} from "react";
 import {createTheme, Theme} from "@mui/material";
 import {lightBlue, orange} from "@mui/material/colors";
 
-export class ThemeStore {
+export class ThemerStore {
     public theme?: Theme = undefined;
 
     constructor() {
@@ -59,5 +59,5 @@ export class ThemeStore {
     }
 }
 
-export const themeStore = new ThemeStore();
+export const themeStore = new ThemerStore();
 export const ThemeContext = createContext(themeStore);
