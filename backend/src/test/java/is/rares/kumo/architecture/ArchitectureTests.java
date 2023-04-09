3fa0a80306b6976e9290526338a58a2ffa1e047f
raresica1234 <colorlord98@gmail.com>
2023-04-09 22:57:31 +0300

    Fixed all architecture tests omg omg omg

--- backend/src/test/java/is/rares/kumo/architecture/ArchitectureTests.java
+++ backend/src/test/java/is/rares/kumo/architecture/ArchitectureTests.java
@@ -1,14 +1,20 @@
 package is.rares.kumo.architecture;
 
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 import is.rares.kumo.KumoApplication;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @AnalyzeClasses(packagesOf = KumoApplication.class, importOptions = {
         ImportOption.DoNotIncludeTests.class,
@@ -16,15 +22,27 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
         ImportOption.DoNotIncludeJars.class
 })
 public class ArchitectureTests {
-    private static final String PACKAGE = "is.rares.kumo";
     @ArchTest
     public static final ArchRule controllersShouldNotAccessDomainObjects = noClasses()
             .that().resideInAPackage("..controller..")
             .should().dependOnClassesThat().resideInAPackage("..domain..");
 
+    private static ArchCondition<JavaMethod> methodAnnotatedWithRequestBodyShouldBeValid =
+            new ArchCondition<>("Methods annotated with @RequestBody should be annotated with @Valid") {
+                @Override
+                public void check(JavaMethod item, ConditionEvents events) {
+                    for (var parameter : item.getParameters())
+                        if (parameter.isAnnotatedWith(RequestBody.class) && !parameter.isAnnotatedWith(Valid.class))
+                            events.add(SimpleConditionEvent.violated(parameter, "Parameters annotated with @RequestBody should be annotated with @Valid \n" + item.toString()));
+                }
+            };
+
+
     @ArchTest
     public static final ArchRule methodsAnnotatedWithRequestBodyShouldHaveValid = methods()
-            .that().areDeclaredInClassesThat().resideInAPackage(PACKAGE)
-            .and().areAnnotatedWith(RequestBody.class)
-            .should().beAnnotatedWith(Valid.class);
+            .that().areDeclaredInClassesThat().resideInAPackage("..controller..")
+            .and().areDeclaredInClassesThat().areAnnotatedWith(RestController.class)
+            .should(methodAnnotatedWithRequestBodyShouldBeValid);
+
+
 }
