16f474cfd838dae154d4aa1ad2ac2cb9fa5008f8
raresica1234 <colorlord98@gmail.com>
2024-04-29 22:40:28 +0300

    Use secure random for generating 2FA codes

--- backend/src/main/java/is/rares/kumo/utils/StringUtils.java
+++ backend/src/main/java/is/rares/kumo/utils/StringUtils.java
@@ -2,19 +2,19 @@ package is.rares.kumo.utils;
 
 import lombok.experimental.UtilityClass;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 @UtilityClass
 public class StringUtils {
-    private final Random randomInstance = new Random();
+    private final SecureRandom randomInstance = new SecureRandom();
 
-    // TODO: Use secure random
     public String generateRandomString(int length) {
         int leftLimit = 48; // '0'
-        int rightLimit = 122; // 'z'
+        int rightLimit = 90; // 'Z'
 
         return randomInstance.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .filter(i -> (i <= 57 || i >= 65) && i <= 90)
                 .limit(length)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
