50d094c324fe5c8c5be3eae0e92d603b89dbe06c
raresica1234 <colorlord98@gmail.com>
2025-02-01 20:42:13 +0200

    Added midnight cronjob to run thumbnail garbage collection

--- backend/src/main/java/is/rares/kumo/service/JobService.java
+++ backend/src/main/java/is/rares/kumo/service/JobService.java
@@ -1,16 +1,23 @@
 package is.rares.kumo.service;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import is.rares.kumo.service.content.ThumbnailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-@Service
+@EnableScheduling
+@Slf4j
 @RequiredArgsConstructor
+@Service
 public class JobService {
     private final ThumbnailService thumbnailService;
 
+    @Scheduled(cron = "0 0 0 * * *")
     public void runThumbnailGarbageCollection() {
+        log.info("Running midnight job");
         thumbnailService.runGarbageCollection();
     }
 }
